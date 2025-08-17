a8aa5708995c352e2320e9e24f853fdbd737ef05
BC76-nTuan <paolonguyentuan1@gmail.com>
2025-08-17 16:48:18 +0700

    update token

--- src/services/config.js
+++ src/services/config.js
@@ -4,6 +4,6 @@ export const http = axios.create({
   timeout: 30000,
   headers: {
     tokenCyberSoft:
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA3NiIsIkhldEhhblN0cmluZyI6IjI1LzA0LzIwMjUiLCJIZXRIYW5UaW1lIjoiMTc0NTUzOTIwMDAwMCIsIm5iZiI6MTcxNzA4ODQwMCwiZXhwIjoxNzQ1Njg2ODAwfQ.bh24gBD3xXGDrdB5FbpBMKC6PooFLXo00Pxus3IZcQQ",
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA3NiIsIkhldEhhblN0cmluZyI6IjI1LzEyLzIwMjUiLCJIZXRIYW5UaW1lIjoiMTc0NTUzOTIwMDAwMCIsIm5iZiI6MTcxNzA4ODQwMCwiZXhwIjoxNzQ1Njg2ODAwfQ.F7TDIFI4W290yYwirY4KdBLLDjRoLtGxZNs9TslEo9A",
   },
 });
